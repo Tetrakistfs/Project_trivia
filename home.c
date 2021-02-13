3d19da6b01ec55cf2a807c6822b6d9d93b4f3068
Payal jain <201210056@nitdelhi.ac.in>
2021-02-13 21:19:45 +0530

    fffff

--- home.c
+++ home.c
@@ -5,8 +5,7 @@
 #include "global_chit_chats.h"
 int main()
 {
-    printf("pppp\n");
-    printf("jjjjj\n");
+    printf("phhh\n");
     char name[31],command;
     int n;
     printf("\e[1;1H\e[2J");
