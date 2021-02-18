9d299d5afcd376d9ce51efcb2ee66d6804e068af
Payal jain <201210056@nitdelhi.ac.in>
2021-02-18 16:47:22 +0530

    updated 18/02/2021

--- anime.h
+++ anime.h
@@ -6,80 +6,284 @@ int anime()
 {
     int score = 0;
     char choice[15];
+    printf("\e[1;1H\e[2J");
     printf("1.Which anime also became a popular trading card game and one of the first augmented reality games to hit the market?\n\tA.\tPokemon\n\tB.\tCase Closed\n\tC.\tMagi\n\tD.\tKill la Kill\n");
     printf("Choice ->");
     scanf(" %c", &choice[0]);
     if(toupper(choice[0]) == 'A')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is A\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("2.\"Fullmetal Alchemist\" is an action-packed story centering on two brothers who lost more than one limb in a ritual to do what?\n\tA.\tgrow their power exponentially\n\tB.\tclose the portal to Hell\n\tC.\tcreate a homonculus\n\tD.\tbring back their dead mother\n");
     printf("Choice ->");   
     scanf(" %c", &choice[1]);
     if(toupper(choice[1]) == 'D')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("3.Which anime features a protagonist able to kill instantly by writing a person's name down in his notebook?\n\tA.\tDeath Note\n\tB.\tClannad\n\tC.\tFull Metal Panic\n\tD.\tHellsing\n");
     printf("Choice ->");
     scanf(" %c", &choice[2]);
     if(toupper(choice[2]) == 'A')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is A\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("4.Around what concept is the hit show \"One Punch Man\" designed?\n\tA.\ta hero who defeats his enemies with a single punch\n\tB.\ta hero who is knocked out after taking one blow\n\tC.\ta hero who saves the universe with his flurry of punches\n\tD.\ta hero who only kicks his enemies to defeat them\n");
     printf("Choice ->");
     scanf(" %c", &choice[3]);
     if(toupper(choice[3]) == 'A')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is A\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("5.Which famous anime produced the term \"Super Saiyan\"?\n\tA.\tBerserk\n\tB.\tDragon Ball Z\n\tC.\tToradora\n\tD.\tSoul eater\n");
     printf("Choice ->");
     scanf(" %c", &choice[4]);
     if(toupper(choice[4]) == 'B')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is B\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("6.In \"Naruto,\" the main character (a ninja) wears what color?\n\tA.\tbright blue\n\tB.\twhite\n\tC.\tmuted red\n\tD.\tbright orange\n");
     printf("Choice ->");
     scanf(" %c", &choice[5]);
     if(toupper(choice[5]) == 'D')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("7.In which Miyazaki film do the characters search for an ancient floating castle?\n\tA.\tPorco Rosso\n\tB.\tMy Neighbor Totoro\n\tC.\tNausicaa of the Valley of the Wind\n\tD.\tCastle in the Sky\n");
     printf("Choice ->");
     scanf(" %c", &choice[6]);
     if(toupper(choice[6]) == 'D')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("8.what kind of person Naruto is?\n\tA.\tA ninja\n\tB.\tA knight\n\tC.\tA sumrai\n\tD.\tA tree surgeon\n");
     printf("Choice ->");
     scanf(" %c", &choice[7]);
     if(toupper(choice[7]) == 'A')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is A\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("9.What is the last name of Edward and Alphonse in the Fullmetal Alchemist series?\n\tA.\tAkerman\n\tB.\tElric\n\tC.\tJeager\n\tD.\tYagami\n");
     printf("Choice ->");
     scanf(" %c", &choice[8]);
     if(toupper(choice[8]) == 'B')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is B\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("10.What two companies currently hold copyrights to \"Dragon Ball\", \"Dragon Ball Z\", and \"Dragon Ball GT\"?\n\tA.\tPioneer And Bird\n\tB.\tToei And Akira Toriyama\n\tC.\tFunimation And Toei\n\tD.\tFunimation And Pioneer\n");
     printf("Choice ->");
     scanf(" %c", &choice[9]);
     if(toupper(choice[9]) == 'C')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is C\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
     printf("11.Which shinigami dropped his notebook in the human world?\n\tA.\tGrell Sutcliff\n\tB.\tRem\n\tC.\tRyuk\n\tD.\tImiyer\n");
     printf("Choice ->");
     scanf(" %c", &choice[10]);
     if(toupper(choice[10]) == 'C')
-    {score = score+100;}
-    // printf("12.?\n\tA.\tCaptain America\n\tB.\tThe Hulk\n\tC.\tVision\n\tD.\tIron Man\n");
-    // printf("Choice ->");
-    // scanf(" %c", &choice[11]);
-    // if(toupper(choice[11]) == 'A')
-    // {score = score+100;}
-    // printf("13.This Avenger friend 'hid' in Wakanda. Which one did this in vibranium land?\n\tA.\tPeter Quill\n\tB.\tScott Lang\n\tC.\tSam Wilson\n\tD.\tBucky Barnes\n");
-    // printf("Choice ->");
-    // scanf(" %c", &choice[12]);
-    // if(toupper(choice[12]) == 'D')
-    // {score = score+100;}
-    // printf("14.Vision came from which of the Infinity Stones?\n\tA.\tMind stone\n\tB.\tTime stone\n\tC.\tSoul stone\n\tD.\tPower stone\n");
-    // printf("Choice ->");
-    // scanf(" %c", &choice[13]);
-    // if(toupper(choice[13]) == 'A')
-    // {score = score+100;}
-    // printf("15.If the reality stone is red and the power stone is purple, what colors are the soul and space stones?\n\tA.\tGreen and pink\n\tB.\tBlack and green\n\tC.\tBrown and gray\n\tD.\tOrange and blue\n");
-    // printf("Choice ->");
-    // scanf(" %c", &choice[14]);
-    // if(toupper(choice[14]) == 'D')
-    {score = score+100;}
-    printf("Congrants!!! Your Score is %d\n" , score);
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is C\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("12.What two companies currently hold copyrights to \"Dragon Ball\", \"Dragon Ball Z\", and \"Dragon Ball GT\"\n\tA.\tPioneer And Bird\n\tB.\tToei And Akira Toriyama\n\tC.\tFunimation And Toei\n\tD.\tFunimation And Pioneer\n");
+    printf("Choice ->");
+    scanf(" %c", &choice[11]);
+    if(toupper(choice[11]) == 'C')
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is C\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("13.For most of the Hundred Year War, where was Avatar Aang in the series \"Avatar\"?\n\tA.\tIn the bookies\n\tB.\tDoing his big shop\n\tC.\tIn a dark Cave\n\tD.\tFrozen in an iceberg\n");
+    printf("Choice ->");
+    scanf(" %c", &choice[12]);
+    if(toupper(choice[12]) == 'D')
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("14.What is the name of Great Uncle \"Grunkle\" Stan’s shack in \"Gravity falls\".?\n\tA.\tThe Mystery Shack\n\tB.\tThe Misery Shack\n\tC.\tThe History Shack\n\tD.\tThe House of Horrors\n");
+    printf("Choice ->");
+    scanf(" %c", &choice[13]);
+    if(toupper(choice[13]) == 'A')
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is A\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("15.What is 'anime' the Japanese word for?\n\tA.\tCartoon\n\tB.\tDrawing\n\tC.\tStory\n\tD.\tAnimation\n");
+    printf("Choice ->");
+    scanf(" %c", &choice[14]);
+    if(toupper(choice[14]) == 'D')
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    if(score>1000)
+    {
+        printf("Congrants!!! Your Score is %d\n" , score);
+        printf("You Rocked it!!\n");
+    }
+    else if(score>500)
+    {
+        printf("Congrants!!! Your Score is %d\n" ,score);
+        printf("Good Performance\n");
+    }
+    else
+    {
+        printf("Your Score is %d\n" ,score);
+        printf("Better luck next time\n");
+    }
 }
--- gaming.h
+++ gaming.h
@@ -6,80 +6,298 @@ int gaming()
 {
     int score = 0;
     char choice[15];
+    printf("\e[1;1H\e[2J");
     printf("1.NES was short for?\n\tA.\tNever Ending Simulation\n\tB.\tNew Enterprise System\n\tC.\tNot Ever Still\n\tD.\tNintendo Entertainment System\n");
     printf("Choice ->");
     scanf(" %c", &choice[0]);
     if(toupper(choice[0]) == 'D')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("2.In what game series are the Golden Rings used as Life-energy and Money?\n\tA.\tSuper Mario Bros\n\tB.\tSonic the Hedgehog\n\tC.\tHarvest Moon\n\tD.\tCeleste\n");
     printf("Choice ->");   
     scanf(" %c", &choice[1]);
     if(toupper(choice[1]) == 'B')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is B\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("3.Who is the Elven Sword-Wielding, Green Clothed Hero in the Legend of the Zelda Series?\n\tA.\tLink\n\tB.\tKai\n\tC.\tLuigi\n\tD.\tKain\n");
     printf("Choice ->");
     scanf(" %c", &choice[2]);
     if(toupper(choice[2]) == 'A')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is A\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("4.What is the name of Mario's dinosaur sidekick?\n\tA.\tGon\n\tB.\tYoshi\n\tC.\tMushy\n\tD.\tKipo\n");
     printf("Choice ->");
     scanf(" %c", &choice[3]);
     if(toupper(choice[3]) == 'B')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is B\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("5.What are the professions of Mario and his brother Luigi?\n\tA.\tGarbage men\n\tB.\tDancers\n\tC.\tHeroes\n\tD.\tPlumbers\n");
     printf("Choice ->");
     scanf(" %c", &choice[4]);
     if(toupper(choice[4]) == 'D')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("6.Characters in videogames who cannot be outrightly controlled by players are acronymed as what?\n\tA.\tWoWs\n\tB.\tNPCs\n\tC.\tMMORPGs\n\tD.\tRPGs\n");
     printf("Choice ->");
     scanf(" %c", &choice[5]);
     if(toupper(choice[5]) == 'B')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is B\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("7.Which videogame holds the record for having the highest budget ever to produce?\n\tA.\tGrand Theft Auto 5\n\tB.\tFinal Fantasy VII\n\tC.\tStar Wars: Old Republic\n\tD.\tKingdom Hearts III\n");
     printf("Choice ->");
     scanf(" %c", &choice[6]);
     if(toupper(choice[6]) == 'A')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is A\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("8.Which item made Mario invincible in \"Super Mario Bros.\"?\n\tA.\tMushroom\n\tB.\tFire Flower\n\tC.\tStarman\n\tD.\tYoshi's Egg\n");
     printf("Choice ->");
     scanf(" %c", &choice[7]);
     if(toupper(choice[7]) == 'C')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is C\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("9.What is the name of the first-ever videogame?\n\tA.\tPong\n\tB.\tPac-Man\n\tC.\tTennis for Two\n\tD.\tAsteroids\n");
     printf("Choice ->");
     scanf(" %c", &choice[8]);
     if(toupper(choice[8]) == 'C')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is C\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("10.How many buttons were on the Original NES controller(including the D-pad)?\n\tA.\t4\n\tB.\t8\n\tC.\t10\n\tD.\t15\n");
     printf("Choice ->");
     scanf(" %c", &choice[9]);
     if(toupper(choice[9]) == 'B')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is B\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("11.In what series were Dracula and The Grim Reaper Recurring Villians?\n\tA.\tSoul Reaver\n\tB.\tCastlevania\n\tC.\tDark Souls\n\tD.\tBloodbourne\n");
     printf("Choice ->");
     scanf(" %c", &choice[10]);
     if(toupper(choice[10]) == 'B')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is B\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("12.What is the name of the legendary videogame designer who created the Mario franchise?\n\tA.\tFusajiro Yamauchi\n\tB.\tTetsuo Hara\n\tC.\tAlexey Pajitnov\n\tD.\tShigeru Miyamoto\n");
     printf("Choice ->");
     scanf(" %c", &choice[11]);
     if(toupper(choice[11]) == 'D')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("13.How many form does 'The Final Boss of the Legend of Dragoon' Have?\n\tA.\t3\n\tB.\t5\n\tC.\t7\n\tD.\t10\n");
     printf("Choice ->");
     scanf(" %c", &choice[12]);
     if(toupper(choice[12]) == 'C')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is C\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("14.What part of an XBOX 360 causes the \"Red Ring of Death\"?\n\tA.\tGraphics Card\n\tB.\tHeat Sink\n\tC.\tThe Processor\n\tD.\tBoth A and B\n");
     printf("Choice ->");
     scanf(" %c", &choice[13]);
     if(toupper(choice[13]) == 'D')
-    {score = score+100;}
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    printf("Moving to next question ");
+        for(int i=2;i>0;i--)
+        {
+            printf("%d..", i);
+            sleep(1);
+        }
+    printf("\e[1;1H\e[2J");
     printf("15What videogame featured the first Easter egg?\n\tA.\tCentipede\n\tB.\tAsteroids\n\tC.\tSpace Invaders\n\tD.\tAdventure\n");
     printf("Choice ->");
     scanf(" %c", &choice[14]);
     if(toupper(choice[14]) == 'D')
-    {score = score+100;}
-    printf("Congrants!!! Your Score is %d\n" , score);
+    {
+        score = score+100;
+        printf("Correct Answer!!!");
+    }
+    else
+    {
+        printf("OOPS!! WRONG ANSWER\nCorrect answer is D\n");
+    }
+    if(score>1000)
+    {
+        printf("Congrants!!! Your Score is %d\n" , score);
+        printf("You Rocked it!!\n");
+    }
+    else if(score>500)
+    {
+        printf("Congrants!!! Your Score is %d\n" ,score);
+        printf("Good Performance\n");
+    }
+    else
+    {
+        printf("Your Score is %d\n" ,score);
+        printf("Better luck next time\n");
+    }
 }
